4422e998dd1c2af49cca77cd4c80e68a75959aa1
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-11-11 08:57:05 +0545

    a very straight forward simulated auth

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 const { connectDatabase } = require('../config/db')
 
 module.exports = async (req, res) => {
-  const db = await connectDatabase()
-  const collection = await db.collection('urls')
-  const urls = await collection.find().toArray()
-  res.send({ urls })
+  if (req.query.s === process.env.SECRET_KEY) {
+    const db = await connectDatabase()
+    const collection = await db.collection('urls')
+    const urls = await collection.find().toArray()
+    return res.send({ urls })
+  }
+  return res
+    .status(401)
+    .send({ message: 'Authentication Needed', success: false })
 }
