e8931ea380cd2f0e7039d8d0b1098864186bddf7
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-11-11 09:05:23 +0545

    link clicks tracking

--- api/redirect.js
+++ api/redirect.js
@@ -4,5 +4,7 @@ module.exports = async (req, res) => {
   const db = await connectDatabase()
   const collection = await db.collection('urls')
   const result = await collection.findOne({ short: req.query.s })
+  result.clicks++
+  result.save()
   res.redirect(result.long)
 }
