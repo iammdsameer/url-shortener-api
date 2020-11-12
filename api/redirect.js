3c9d78e80d859b42cf00e85523d87ea923bab4bb
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-11-12 14:00:22 +0545

    wip: clicks not being saved to db

--- api/redirect.js
+++ api/redirect.js
@@ -3,8 +3,13 @@ const { connectDatabase } = require('../config/db')
 module.exports = async (req, res) => {
   const db = await connectDatabase()
   const collection = await db.collection('urls')
-  const result = await collection.findOne({ short: req.query.s })
-  result.clicks++
-  result.save()
+  // const result = await collection.findOne({ short: req.query.s })
+  // result.clicks++
+  // collection.save()
+  // res.redirect(result.long)
+  await collection.findOneAndUpdate(
+    { short: req.query.s },
+    { $set: { clicks: res.clicks++ } }
+  )
   res.redirect(result.long)
 }
