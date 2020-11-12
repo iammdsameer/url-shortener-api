ee37d223e4d46d402892d8f9fc72051dbbb83986
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-11-12 14:12:38 +0545

    minor fixes

--- api/redirect.js
+++ api/redirect.js
@@ -3,7 +3,7 @@ const { connectDatabase } = require('../config/db')
 module.exports = async (req, res) => {
   const db = await connectDatabase()
   const collection = await db.collection('urls')
-  // const result = await collection.findOne({ short: req.query.s })
+  const result = await collection.findOne({ short: req.query.s })
   // result.clicks++
   // collection.save()
   // res.redirect(result.long)
