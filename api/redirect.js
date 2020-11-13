273e131c9da30ff30c45b96aacf7d592ef5c29d7
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-11-13 19:28:23 +0545

    feature: click funtionality

--- api/redirect.js
+++ api/redirect.js
@@ -4,12 +4,7 @@ module.exports = async (req, res) => {
   const db = await connectDatabase()
   const collection = await db.collection('urls')
   const result = await collection.findOne({ short: req.query.s })
-  // result.clicks++
-  // collection.save()
-  // res.redirect(result.long)
-  // await collection.findOneAndUpdate(
-  //   { short: req.query.s },
-  //   { $set: { clicks: res.clicks++ } }
-  // )
+  let clicks = result.clicks + 1
+  await collection.findOneAndUpdate({ short: req.query.s }, { clicks })
   res.redirect(result.long)
 }
